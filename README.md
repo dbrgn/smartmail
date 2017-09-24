e52c7a898667806ff6bf4bd1b7d181bc9ef0faed
Danilo Bargen <mail@dbrgn.ch>
2017-09-24 11:13:20 +0200

    Threema settings

--- README.md
+++ README.md
@@ -11,6 +11,10 @@ Export the following environment variables:
 
 - `TTN_APP_ID`: The Things Network App ID
 - `TTN_ACCESS_KEY`: The Things Network Access Key
+- `THREEMA_FROM`: Your Threema Gateway ID, 8 characters starting with `*`
+- `THREEMA_TO`: A comma separated list of recipient Threema IDs
+- `THREEMA_SECRET`: The Threema Gateway API secret
+- `THREEMA_PRIVATE_KEY`: The hex encoded private key of your Gateway ID
 
 If you don't want to manually export environment variables, you can also write
 them into a `.env` file (format: `KEY=value`, one entry per line).
--- src/config.rs
+++ src/config.rs
@@ -4,13 +4,26 @@ use std::env;
 pub struct Config {
     pub ttn_app_id: String,
     pub ttn_access_key: String,
+
+    pub threema_from: String,
+    pub threema_to: Vec<String>,
+    pub threema_secret: String,
+    pub threema_private_key: String,
+}
+
+fn get_env_var(name: &str) -> Result<String, String> {
+    env::var(name).map_err(|_| format!("Missing {} env var", name))
 }
 
 impl Config {
     pub fn init() -> Result<Config, String> {
         Ok(Config {
-            ttn_app_id: env::var("TTN_APP_ID").map_err(|_| "Missing TTN_APP_ID env var")?,
-            ttn_access_key: env::var("TTN_ACCESS_KEY").map_err(|_| "Missing TTN_ACCESS_KEY env var")?,
+            ttn_app_id: get_env_var("TTN_APP_ID")?,
+            ttn_access_key: get_env_var("TTN_ACCESS_KEY")?,
+            threema_from: get_env_var("THREEMA_FROM")?,
+            threema_to: get_env_var("THREEMA_TO")?.split(',').map(|s| s.to_owned()).collect(),
+            threema_secret: get_env_var("THREEMA_SECRET")?,
+            threema_private_key: get_env_var("THREEMA_PRIVATE_KEY")?,
         })
     }
 }
